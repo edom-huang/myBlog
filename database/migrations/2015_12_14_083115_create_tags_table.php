d37b8576ac7775e4b58e77d7f1bc22dc98464f75
edom_huang <859867160@qq.com>
2015-12-18 23:10:22 +0800

    Update 2015_12_14_083115_create_tags_table.php
    modified layout

--- database/migrations/2015_12_14_083115_create_tags_table.php
+++ database/migrations/2015_12_14_083115_create_tags_table.php
@@ -19,7 +19,7 @@ class CreateTagsTable extends Migration
             $table->string('subtitle');
             $table->string('page_image');
             $table->string('meta_description');
-            $table->string('layouts')->default('blog.layouts.index');
+            $table->string('layout')->default('blog.layouts.index');
             $table->boolean('reverse_direction');
             $table->timestamps();
         });
